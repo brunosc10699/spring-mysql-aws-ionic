377c9c44aa92fcb2722b9436971920218b331255
brunosc10699 <brunosc10699@gmail.com>
2021-08-12 12:05:11 -0300

    project started

---
+++ src/test/java/com/bruno/ordering/OrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
