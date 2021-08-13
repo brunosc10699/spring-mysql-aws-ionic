94d9fbf72a1abc3e20b54539370cf4d75deb6c0a
brunosc10699 <brunosc10699@gmail.com>
2021-08-13 15:14:37 -0300

    spring security configured to allow access to h2 console

---
+++ src/main/java/com/bruno/ordering/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.bruno.ordering.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test"))
+            http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
+        http.cors().and().csrf().disable();
+    }
+}
